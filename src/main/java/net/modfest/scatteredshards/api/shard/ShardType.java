acd784f19d9869dddce6f0edb6a80cc2d597160b
Skye Prince <spiralixp@gmail.com>
2023-08-20 10:24:05 -0700

    add new sounds

--- src/main/java/net/modfest/scatteredshards/api/shard/ShardType.java
+++ src/main/java/net/modfest/scatteredshards/api/shard/ShardType.java
@@ -1,7 +1,5 @@
 package net.modfest.scatteredshards.api.shard;
 
-import java.util.Optional;
-
 import com.google.gson.JsonObject;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.particle.ParticleType;
@@ -9,21 +7,23 @@ import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
 import net.modfest.scatteredshards.ScatteredShards;
 import net.modfest.scatteredshards.api.ScatteredShardsAPI;
 
+import java.util.Optional;
+
 public record ShardType(int textColor, int glowColor, Optional<ParticleType<?>> collectParticle, Optional<SoundEvent> collectSound) {
 
 	public static final SoundEvent COLLECT_VISITOR_SOUND = SoundEvent.createVariableRangeEvent(ScatteredShards.id("collect_visitor"));
 	public static final SoundEvent COLLECT_CHALLENGE_SOUND = SoundEvent.createVariableRangeEvent(ScatteredShards.id("collect_challenge"));
+	public static final SoundEvent COLLECT_SECRET_SOUND = SoundEvent.createVariableRangeEvent(ScatteredShards.id("collect_secret"));
 	
 	public static final ShardType VISITOR = new ShardType(0x6DE851, 0x00FF48, Optional.of(ParticleTypes.TOTEM_OF_UNDYING), Optional.of(COLLECT_VISITOR_SOUND));
 	public static final ShardType CHALLENGE = new ShardType(0x5174E8, 0x0026FF, Optional.of(ParticleTypes.GLOW), Optional.of(COLLECT_CHALLENGE_SOUND));
-	public static final ShardType SECRET = new ShardType(0xEB4034, 0xFF0088, Optional.of(ParticleTypes.WITCH), Optional.of(SoundEvents.UI_TOAST_CHALLENGE_COMPLETE));
+	public static final ShardType SECRET = new ShardType(0xEB4034, 0xFF0088, Optional.of(ParticleTypes.WITCH), Optional.of(COLLECT_SECRET_SOUND));
 	public static final ShardType MISSING = new ShardType(0xFFFFFF, 0xFF00FF, Optional.empty(), Optional.empty());
 
 	public static final Identifier MISSING_ID = ScatteredShards.id("missing");
@@ -93,6 +93,7 @@ public record ShardType(int textColor, int glowColor, Optional<ParticleType<?>> 
 	public static void register() {
 		Registry.register(Registries.SOUND_EVENT, COLLECT_VISITOR_SOUND.getId(), COLLECT_VISITOR_SOUND);
 		Registry.register(Registries.SOUND_EVENT, COLLECT_CHALLENGE_SOUND.getId(), COLLECT_CHALLENGE_SOUND);
+		Registry.register(Registries.SOUND_EVENT, COLLECT_SECRET_SOUND.getId(), COLLECT_SECRET_SOUND);
 		
 		ScatteredShardsAPI.registerShardType(ScatteredShards.id("visitor"), VISITOR);
 		ScatteredShardsAPI.registerShardType(ScatteredShards.id("challenge"), CHALLENGE);
