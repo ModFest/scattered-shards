c21f24134b62fea4036283f3b98e0ed00be29805
Falkreon <falkreon@gmail.com>
2023-08-21 07:59:00 -0500

    Correct indentation in tablet item

--- src/main/java/net/modfest/scatteredshards/item/ShardTablet.java
+++ src/main/java/net/modfest/scatteredshards/item/ShardTablet.java
@@ -16,24 +16,24 @@ import net.modfest.scatteredshards.component.ScatteredShardsComponents;
 
 public class ShardTablet extends Item {
 
-    public ShardTablet(Settings settings) {
-        super(settings);
-    }
+	public ShardTablet(Settings settings) {
+		super(settings);
+	}
 
-    @ClientOnly
-    // TODO: sneak interact on another player opens their collection
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (!world.isClient) {
-            return TypedActionResult.pass(user.getStackInHand(hand));
-        }
-        final var library = ScatteredShardsComponents.getShardLibrary(world);
-        final var collection = ScatteredShardsComponents.getShardCollection(user);
-        final var client = MinecraftClient.getInstance();
-        client.send(() -> {
-            client.setScreen(new ShardTabletGuiDescription.Screen(collection, library));
-            client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.ITEM_BOOK_PAGE_TURN, 1.0f, 1.0f));
-        });
-        return TypedActionResult.success(user.getStackInHand(hand));
-    }
+	@ClientOnly
+	// TODO: sneak interact on another player opens their collection
+	@Override
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+		if (!world.isClient) {
+			return TypedActionResult.pass(user.getStackInHand(hand));
+		}
+		final var library = ScatteredShardsComponents.getShardLibrary(world);
+		final var collection = ScatteredShardsComponents.getShardCollection(user);
+		final var client = MinecraftClient.getInstance();
+		client.send(() -> {
+			client.setScreen(new ShardTabletGuiDescription.Screen(collection, library));
+			client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.ITEM_BOOK_PAGE_TURN, 1.0f, 1.0f));
+		});
+		return TypedActionResult.success(user.getStackInHand(hand));
+	}
 }
