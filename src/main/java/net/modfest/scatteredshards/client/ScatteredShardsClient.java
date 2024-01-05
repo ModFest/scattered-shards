39ac0a64ea5ae6e9cd62e14bca36d7b3fde480e8
Skye Prince <spiralixp@gmail.com>
2024-01-04 17:00:38 -0800

    fix server crash

--- src/main/java/net/modfest/scatteredshards/client/ScatteredShardsClient.java
+++ src/main/java/net/modfest/scatteredshards/client/ScatteredShardsClient.java
@@ -6,6 +6,7 @@ import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.toast.Toast;
 
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.modfest.scatteredshards.ScatteredShards;
@@ -14,6 +15,7 @@ import net.modfest.scatteredshards.ScatteredShardsContent;
 import net.modfest.scatteredshards.api.ScatteredShardsAPI;
 import net.modfest.scatteredshards.api.shard.Shard;
 import net.modfest.scatteredshards.api.shard.ShardType;
+import net.modfest.scatteredshards.client.screen.ShardTabletGuiDescription;
 import net.modfest.scatteredshards.networking.ScatteredShardsNetworking;
 
 public class ScatteredShardsClient implements ClientModInitializer {
@@ -59,5 +61,16 @@ public class ScatteredShardsClient implements ClientModInitializer {
 				Text.translatable(SHARD_MODIFY_TOAST_KEY + "." + (success ? "success" : "fail"), shardId)
 		);
 		MinecraftClient.getInstance().getToastManager().add(toast);
+	}
+
+	public static void openShardTablet() {
+		final var client = MinecraftClient.getInstance();
+		client.send(() -> {
+			final var library = ScatteredShardsAPI.getClientLibrary();
+			final var collection = ScatteredShardsAPI.getClientCollection();
+
+			client.setScreen(new ShardTabletGuiDescription.Screen(collection, library));
+			client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.ITEM_BOOK_PAGE_TURN, 1.0f, 1.0f));
+		});
 	}
 }
--- src/main/java/net/modfest/scatteredshards/item/ShardTablet.java
+++ src/main/java/net/modfest/scatteredshards/item/ShardTablet.java
@@ -2,17 +2,13 @@ package net.modfest.scatteredshards.item;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
-import net.modfest.scatteredshards.api.ScatteredShardsAPI;
-import net.modfest.scatteredshards.client.screen.ShardTabletGuiDescription;
+import net.modfest.scatteredshards.client.ScatteredShardsClient;
 
 public class ShardTablet extends Item {
 
@@ -24,14 +20,7 @@ public class ShardTablet extends Item {
 	// TODO: sneak interact on another player opens their collection
 	@Override
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-		final var client = MinecraftClient.getInstance();
-		client.send(() -> {
-			final var library = ScatteredShardsAPI.getClientLibrary();
-			final var collection = ScatteredShardsAPI.getClientCollection();
-			
-			client.setScreen(new ShardTabletGuiDescription.Screen(collection, library));
-			client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.ITEM_BOOK_PAGE_TURN, 1.0f, 1.0f));
-		});
+		ScatteredShardsClient.openShardTablet();
 		return TypedActionResult.success(user.getStackInHand(hand));
 	}
 }
