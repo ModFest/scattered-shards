fecdd84b189cab006d561e55b1a76f310b2f900c
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-03-21 16:36:47 +0100

    Clean up code

--- src/main/java/net/modfest/scatteredshards/networking/C2SCreateShardInstant.java
+++ src/main/java/net/modfest/scatteredshards/networking/C2SCreateShardInstant.java
@@ -1,16 +1,14 @@
 package net.modfest.scatteredshards.networking;
 
-import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.modfest.scatteredshards.ScatteredShards;
 import net.modfest.scatteredshards.api.ScatteredShardsAPI;
-import net.modfest.scatteredshards.api.impl.ShardLibraryPersistentState;
+import net.modfest.scatteredshards.api.ShardLibrary;
 import net.modfest.scatteredshards.api.shard.Shard;
 import net.modfest.scatteredshards.block.ShardBlock;
 
@@ -22,36 +20,16 @@ public record C2SCreateShardInstant(Identifier shardId, Shard shard) implements 
 	public static final PacketCodec<RegistryByteBuf, C2SCreateShardInstant> PACKET_CODEC = PacketCodec.tuple(Identifier.PACKET_CODEC, C2SCreateShardInstant::shardId, Shard.PACKET_CODEC, C2SCreateShardInstant::shard, C2SCreateShardInstant::new);
 
 	public static void receive(C2SCreateShardInstant payload, ServerPlayNetworking.Context context) {
-		// TODO: This is mostly identical to C2SModifyShard, no need for so much duplicate code
+		boolean success = C2SModifyShard.modify(context.player(), payload.shardId, payload.shard);
 
-		MinecraftServer server = context.player().getServer();
-		server.execute(() -> {
-			boolean success = server.isSingleplayer() || Permissions.check(context.player(), ScatteredShardsAPI.MODIFY_SHARD_PERMISSION, 1);
+		if (!success) {
+			return;
+		}
 
-			//Let the sender know of success or failure before a shard update comes through
-			ServerPlayNetworking.send(context.player(), new S2CModifyShardResult(payload.shardId(), success));
+		ShardLibrary library = ScatteredShardsAPI.getServerLibrary();
 
-			if (!success) {
-				return;
-			}
-
-			//Update our serverside library
-			var library = ScatteredShardsAPI.getServerLibrary();
-			library.shards().put(payload.shardId(), payload.shard());
-			library.shardSets().put(payload.shard().sourceId(), payload.shardId());
-
-			//Make sure the NBT gets written on next world-save
-			ShardLibraryPersistentState.get(server).markDirty();
-
-			//Update everyone's client libraries with the new shard
-			S2CSyncShard syncShard = new S2CSyncShard(payload.shardId(), payload.shard());
-			for (ServerPlayerEntity otherPlayer : server.getPlayerManager().getPlayerList()) {
-				ServerPlayNetworking.send(otherPlayer, syncShard);
-			}
-
-			var itemStack = ShardBlock.createShardBlock(library, payload.shardId(), false, 0.5f, 0.5f);
-			context.player().getInventory().offerOrDrop(itemStack);
-		});
+		ItemStack itemStack = ShardBlock.createShardBlock(library, payload.shardId(), false, 0.5f, 0.5f);
+		context.player().getInventory().offerOrDrop(itemStack);
 	}
 
 	@Override
--- src/main/java/net/modfest/scatteredshards/networking/C2SModifyShard.java
+++ src/main/java/net/modfest/scatteredshards/networking/C2SModifyShard.java
@@ -21,30 +21,38 @@ public record C2SModifyShard(Identifier shardId, Shard shard) implements CustomP
 	public static final PacketCodec<RegistryByteBuf, C2SModifyShard> PACKET_CODEC = PacketCodec.tuple(Identifier.PACKET_CODEC, C2SModifyShard::shardId, Shard.PACKET_CODEC, C2SModifyShard::shard, C2SModifyShard::new);
 
 	public static void receive(C2SModifyShard payload, ServerPlayNetworking.Context context) {
-		MinecraftServer server = context.player().getServer();
-		server.execute(() -> {
-			boolean success = server.isSingleplayer() || Permissions.check(context.player(), ScatteredShardsAPI.MODIFY_SHARD_PERMISSION, 1);
+		modify(context.player(), payload.shardId, payload.shard);
+	}
 
+	public static boolean modify(ServerPlayerEntity player, Identifier shardId, Shard shard) {
+		MinecraftServer server = player.getServer();
+		assert server != null;
+
+		boolean success = server.isSingleplayer() || Permissions.check(player, ScatteredShardsAPI.MODIFY_SHARD_PERMISSION, 1);
+
+		server.execute(() -> {
 			//Let the sender know of success or failure before a shard update comes through
-			ServerPlayNetworking.send(context.player(), new S2CModifyShardResult(payload.shardId(), success));
+			ServerPlayNetworking.send(player, new S2CModifyShardResult(shardId, success));
 
 			if (!success) {
 				return;
 			}
 
 			//Update our serverside library
-			ScatteredShardsAPI.getServerLibrary().shards().put(payload.shardId(), payload.shard());
-			ScatteredShardsAPI.getServerLibrary().shardSets().put(payload.shard().sourceId(), payload.shardId());
+			ScatteredShardsAPI.getServerLibrary().shards().put(shardId, shard);
+			ScatteredShardsAPI.getServerLibrary().shardSets().put(shard.sourceId(), shardId);
 
 			//Make sure the NBT gets written on next world-save
 			ShardLibraryPersistentState.get(server).markDirty();
 
 			//Update everyone's client libraries with the new shard
-			S2CSyncShard syncShard = new S2CSyncShard(payload.shardId(), payload.shard());
+			S2CSyncShard syncShard = new S2CSyncShard(shardId, shard);
 			for (ServerPlayerEntity otherPlayer : server.getPlayerManager().getPlayerList()) {
 				ServerPlayNetworking.send(otherPlayer, syncShard);
 			}
 		});
+
+		return success;
 	}
 
 	@Override
